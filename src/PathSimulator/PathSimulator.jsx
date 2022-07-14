60f274f1b273d758fcc2a4d63f0c82764d1f6076
amit <amit.ethan.halpert@gmail.com>
2022-07-14 12:52:17 +0300

    astar.js: implemented the astar algo

--- src/PathSimulator/PathSimulator.jsx
+++ src/PathSimulator/PathSimulator.jsx
@@ -1,12 +1,11 @@
-
 // import components
-import React, {Component} from 'react';
-import Node from './Node/Node';
-import {dijkstra, getShortestDijkstra} from '../algorithms/dijkstra';
-import {bfs, getShortestPathBFS} from '../algorithms/bfs';
-import {dfs, getShortestPathDFS} from '../algorithms/dfs';
-import './PathSimulator.css';
-
+import React, { Component } from "react";
+import Node from "./Node/Node";
+import { dijkstra, getShortestDijkstra } from "../algorithms/dijkstra";
+import { bfs, getShortestPathBFS } from "../algorithms/bfs";
+import { dfs, getShortestPathDFS } from "../algorithms/dfs";
+import { aastar, getShortestAstar} from "../algorithms/astar";
+import "./PathSimulator.css";
 
 // constants
 const NUMBER_ROW = 26;
@@ -19,43 +18,45 @@ const FINISH_NODE_COL = 35;
 
 export default class PathSimulator extends Component {
   state = {
-      matrix: [],
-      mouseIsPressed: false,
-      mainIsPressed: "",
-      isVisualizing: false,
-      startNode_Pos: [START_NODE_ROW, START_NODE_COL],
-      finishNode_Pos: [FINISH_NODE_ROW, FINISH_NODE_COL],
-  }
-  
+    matrix: [],
+    mouseIsPressed: false,
+    mainIsPressed: "",
+    isVisualizing: false,
+    startNode_Pos: [START_NODE_ROW, START_NODE_COL],
+    finishNode_Pos: [FINISH_NODE_ROW, FINISH_NODE_COL],
+  };
 
   componentDidMount() {
     const { startNode_Pos, finishNode_Pos } = this.state;
-    let matrix = getInitialMatrix(startNode_Pos ,finishNode_Pos);
-    this.setState({matrix});
+    let matrix = getInitialMatrix(startNode_Pos, finishNode_Pos);
+    this.setState({ matrix });
   }
-  
+
   /////////////////
   // user input handling
   /////////////////
 
   // user clicking a node
   handleMouseDown(row, col) {
-    const {matrix, mainIsPressed} = this.state
+    const { matrix, mainIsPressed } = this.state;
     const node = matrix[row][col];
 
-    if(node.isStart === true && node.isFinish === false){
-      this.setState({mainIsPressed: "start"});
+    if (node.isStart === true && node.isFinish === false) {
+      this.setState({ mainIsPressed: "start" });
       node.isStart = false;
     }
-    if(node.isFinish === true && node.isStart === false){
+    if (node.isFinish === true && node.isStart === false) {
       this.setState({ mainIsPressed: "finish" });
       node.isFinish = false;
     }
-    if (mainIsPressed === "" && node.isFinish === false && node.isStart === false) {
+    if (
+      mainIsPressed === "" &&
+      node.isFinish === false &&
+      node.isStart === false
+    ) {
       const newMatrix = MatrixWithWallToggled(matrix, row, col);
       this.setState({ matrix: newMatrix, mouseIsPressed: true });
     }
-
   }
 
   // user hovering over a node
@@ -64,96 +65,94 @@ export default class PathSimulator extends Component {
 
     if (mainIsPressed === "start") {
       const newMatrix = MatrixDynamicNodes(matrix, row, col, "start");
-      this.setState({ matrix: newMatrix});
+      this.setState({ matrix: newMatrix });
     }
 
-  
-    if(mainIsPressed === "finish") {
+    if (mainIsPressed === "finish") {
       const newMatrix = MatrixDynamicNodes(matrix, row, col, "finish");
       this.setState({ matrix: newMatrix });
     }
-    
-  if(mainIsPressed !== "finish" && mainIsPressed !== "start"){
-   if (mouseIsPressed && mainIsPressed === "" && mainIsPressed !== "start") {
-      const newMatrix = MatrixWithWallToggled(matrix, row, col);
-      this.setState({ matrix: newMatrix, mouseIsPressed: true });
-    }
-   }
-  }
 
+    if (mainIsPressed !== "finish" && mainIsPressed !== "start") {
+      if (mouseIsPressed && mainIsPressed === "" && mainIsPressed !== "start") {
+        const newMatrix = MatrixWithWallToggled(matrix, row, col);
+        this.setState({ matrix: newMatrix, mouseIsPressed: true });
+      }
+    }
+  }
 
   handleMouseUp(row, col) {
     const { mainIsPressed, matrix } = this.state;
     if (mainIsPressed === "start") {
-        this.setState({ mainIsPressed: "" });
-        const startNode_Pos = [row, col];
-        const newMatrix = MatrixDynamicNodes(matrix, row, col, "start");
-        this.setState({ mainIsPressed: "", startNode_Pos, matrix: newMatrix });
+      this.setState({ mainIsPressed: "" });
+      const startNode_Pos = [row, col];
+      const newMatrix = MatrixDynamicNodes(matrix, row, col, "start");
+      this.setState({ mainIsPressed: "", startNode_Pos, matrix: newMatrix });
     }
     if (mainIsPressed === "finish") {
-        const finishNode_Pos = [row, col];
-        const newMatrix = MatrixDynamicNodes(matrix, row, col, "finish");
-        this.setState({ mainIsPressed: "", finishNode_Pos, matrix: newMatrix });
+      const finishNode_Pos = [row, col];
+      const newMatrix = MatrixDynamicNodes(matrix, row, col, "finish");
+      this.setState({ mainIsPressed: "", finishNode_Pos, matrix: newMatrix });
     }
     this.setState({ mouseIsPressed: false });
-}
-  
+  }
 
   // user not clicking
   handleMouseLeave(row, col) {
     const { matrix, mainIsPressed } = this.state;
-    if (mainIsPressed === "")
-        return;
+    if (mainIsPressed === "") return;
     let newMatrix = matrix.slice();
     const node = newMatrix[row][col];
     if (mainIsPressed === "start") {
-        const newNode = {
-            ...node,
-            isStart: false,
-            isWall: false
-        }
-        newMatrix[row][col] = newNode;
+      const newNode = {
+        ...node,
+        isStart: false,
+        isWall: false,
+      };
+      newMatrix[row][col] = newNode;
     }
     if (mainIsPressed === "finish") {
-        const newNode = {
-            ...node,
-            isFinish: false,
-            isWall: false
-        }
-        newMatrix[row][col] = newNode;
+      const newNode = {
+        ...node,
+        isFinish: false,
+        isWall: false,
+      };
+      newMatrix[row][col] = newNode;
     }
     this.setState({ matrix: newMatrix });
-}
-
+  }
 
   /////////////////
   // clear functions
   /////////////////
 
-
   clearMatrix = () => {
     const { startNode_Pos, finishNode_Pos } = this.state;
-    const start_X = startNode_Pos[0], start_Y = startNode_Pos[1];
-    const finish_X = finishNode_Pos[0], finish_Y = finishNode_Pos[1];
+    const start_X = startNode_Pos[0],
+      start_Y = startNode_Pos[1];
+    const finish_X = finishNode_Pos[0],
+      finish_Y = finishNode_Pos[1];
     for (let row = 0; row < this.state.matrix.length; row++) {
-        for (let col = 0; col < this.state.matrix[0].length; col++) {
-            if (!((row === start_X && col === start_Y) || (row === finish_X && col === finish_Y))) {
-                document.getElementById(`node-${row}-${col}`).className = "node";
-            }
+      for (let col = 0; col < this.state.matrix[0].length; col++) {
+        if (
+          !(
+            (row === start_X && col === start_Y) ||
+            (row === finish_X && col === finish_Y)
+          )
+        ) {
+          document.getElementById(`node-${row}-${col}`).className = "node";
         }
+      }
     }
-    const newMatrix = getInitialMatrix(startNode_Pos,finishNode_Pos);
-    this.setState({ matrix: newMatrix});
-}
-
-
+    const newMatrix = getInitialMatrix(startNode_Pos, finishNode_Pos);
+    this.setState({ matrix: newMatrix });
+  };
 
   /////////////////
   // algorithms animations
   /////////////////
 
-
-  animateDFS(visitedNodesInOrder, getShortestPathDFS){
+  animateDFS(visitedNodesInOrder, getShortestPathDFS) {
     for (let i = 0; i <= visitedNodesInOrder.length; i++) {
       if (i === visitedNodesInOrder.length) {
         setTimeout(() => {
@@ -164,12 +163,12 @@ export default class PathSimulator extends Component {
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
-          'node node-visited';
+          "node node-visited";
       }, 10 * i);
     }
   }
 
-  animateBFS(visitedNodesInOrder, getShortestPathBFS){
+  animateBFS(visitedNodesInOrder, getShortestPathBFS) {
     for (let i = 0; i <= visitedNodesInOrder.length; i++) {
       if (i === visitedNodesInOrder.length) {
         setTimeout(() => {
@@ -180,7 +179,7 @@ export default class PathSimulator extends Component {
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
-          'node node-visited';
+          "node node-visited";
       }, 10 * i);
     }
   }
@@ -196,20 +195,39 @@ export default class PathSimulator extends Component {
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
-          'node node-visited';
+          "node node-visited";
       }, 10 * i);
     }
   }
 
+  animateAstar(visitedNodesInOrder, getShortestAstar) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          this.animateShortestPath(getShortestAstar);
+        }, 10 * i);
+        return;
+      }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          "node node-visited";
+      }, 10 * i);
+    }
+  }
+
+  
 
 
   visualizeDFS() {
-    const {matrix, startNode_Pos, finishNode_Pos} = this.state;
+    const { matrix, startNode_Pos, finishNode_Pos } = this.state;
     // get the startX and startY
-    const start_X = startNode_Pos[0], start_Y = startNode_Pos[1];
+    const start_X = startNode_Pos[0],
+      start_Y = startNode_Pos[1];
     const startNode = matrix[start_X][start_Y];
     // get the finishX and finishY
-    const finish_X = finishNode_Pos[0], finish_Y = finishNode_Pos[1];
+    const finish_X = finishNode_Pos[0],
+      finish_Y = finishNode_Pos[1];
     const finishNode = matrix[finish_X][finish_Y];
 
     const visitedNodesInOrder = dfs(matrix, startNode, finishNode);
@@ -218,10 +236,12 @@ export default class PathSimulator extends Component {
   }
 
   visualizeBFS() {
-    const {matrix, startNode_Pos, finishNode_Pos} = this.state;
-    const start_X = startNode_Pos[0], start_Y = startNode_Pos[1];
+    const { matrix, startNode_Pos, finishNode_Pos } = this.state;
+    const start_X = startNode_Pos[0],
+      start_Y = startNode_Pos[1];
     const startNode = matrix[start_X][start_Y];
-    const finish_X = finishNode_Pos[0], finish_Y = finishNode_Pos[1];
+    const finish_X = finishNode_Pos[0],
+      finish_Y = finishNode_Pos[1];
     const finishNode = matrix[finish_X][finish_Y];
     const visitedNodesInOrder = bfs(matrix, startNode, finishNode);
     const nodesInShortestPathOrder = getShortestPathBFS(finishNode);
@@ -229,53 +249,77 @@ export default class PathSimulator extends Component {
   }
 
   visualizeDijkstra() {
-    const {matrix, startNode_Pos, finishNode_Pos} = this.state;
-    const start_X = startNode_Pos[0], start_Y = startNode_Pos[1];
+    const { matrix, startNode_Pos, finishNode_Pos } = this.state;
+    const start_X = startNode_Pos[0],
+      start_Y = startNode_Pos[1];
     const startNode = matrix[start_X][start_Y];
-    const finish_X = finishNode_Pos[0], finish_Y = finishNode_Pos[1];
+    const finish_X = finishNode_Pos[0],
+      finish_Y = finishNode_Pos[1];
     const finishNode = matrix[finish_X][finish_Y];
     const visitedNodesInOrder = dijkstra(matrix, startNode, finishNode);
     const nodesInShortestPathOrder = getShortestDijkstra(finishNode);
     this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
   }
 
+  visualizeAstar() {
+    const { matrix, startNode_Pos, finishNode_Pos } = this.state;
+    const start_X = startNode_Pos[0],
+      start_Y = startNode_Pos[1];
+    const startNode = matrix[start_X][start_Y];
+    const finish_X = finishNode_Pos[0],
+      finish_Y = finishNode_Pos[1];
+    const finishNode = matrix[finish_X][finish_Y];
+    try {
+        const visitedNodesInOrder = aastar(matrix, startNode, finishNode);
+        const nodesInShortestPathOrder = getShortestAstar(finishNode);
+        if (nodesInShortestPathOrder.length === 1) {
+            throw "not enough nodes in shortest path"
+        }
 
+        this.animateAstar(visitedNodesInOrder, nodesInShortestPathOrder);
+
+    }catch(error){
+        console.log(error);
+    }
+  }
+  
 
   // animateShortestPath is used for animating every algorithm
-  animateShortestPath(nodesInShortestPathOrder) {
-    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+  animateShortestPath(nodesInShortestPath) {
+    for (let i = 1; i < nodesInShortestPath.length-1; i++) {
       setTimeout(() => {
-        const node = nodesInShortestPathOrder[i];
-        document.getElementById(`node-${node.row}-${node.col}`).className =
-          'node node-shortest-path';
+        const node = nodesInShortestPath[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className = `node node-shortest-path`;
       }, 50 * i);
     }
   }
 
-
   render() {
-    const {matrix, mouseIsPressed} = this.state;
+    const { matrix, mouseIsPressed } = this.state;
 
     return (
       <>
-     <button onClick={() => this.visualizeDijkstra()} class="button-68" >
-       DIJKSTRA
-     </button>
-     <button onClick={() => this.visualizeBFS()} class="button-68" >
-       BFS
-     </button>
-     <button onClick={() => this.visualizeDFS()} class="button-68" >
-       DFS
-     </button>
-     <button onClick={() => this.clearMatrix()} class="button-68" >
-    CLEAR
-    </button>
+        <button onClick={() => this.visualizeAstar()} class="button-68">
+          ASTAR
+        </button>
+        <button onClick={() => this.visualizeDijkstra()} class="button-68">
+          DIJKSTRA
+        </button>
+        <button onClick={() => this.visualizeBFS()} class="button-68">
+          BFS
+        </button>
+        <button onClick={() => this.visualizeDFS()} class="button-68">
+          DFS
+        </button>
+        <button onClick={() => this.clearMatrix()} class="button-68">
+          CLEAR
+        </button>
         <div className="matrix">
           {matrix.map((row, rowIdx) => {
             return (
               <div key={rowIdx}>
                 {row.map((node, nodeIdx) => {
-                  const {row, col, isFinish, isStart, isWall} = node;
+                  const { row, col, isFinish, isStart, isWall } = node;
                   return (
                     <Node
                       col={col}
@@ -286,10 +330,14 @@ export default class PathSimulator extends Component {
                       isWall={isWall}
                       mouseIsPressed={mouseIsPressed}
                       onMouseDown={(row, col) => this.handleMouseDown(row, col)}
-                      onMouseEnter={(row, col) => this.handleMouseEnter(row, col)}
+                      onMouseEnter={(row, col) =>
+                        this.handleMouseEnter(row, col)
+                      }
                       onMouseUp={(row, col) => this.handleMouseUp(row, col)}
-                      onMouseLeave={(row, col) => this.handleMouseLeave(row, col)}
-                      />
+                      onMouseLeave={(row, col) =>
+                        this.handleMouseLeave(row, col)
+                      }
+                    />
                   );
                 })}
               </div>
@@ -301,9 +349,8 @@ export default class PathSimulator extends Component {
   }
 } // end of the PathSimulator
 
-
 /////////////////
-// matrix 
+// matrix
 /////////////////
 
 const getInitialMatrix = (startNode_Pos, finishNode_Pos) => {
@@ -311,16 +358,14 @@ const getInitialMatrix = (startNode_Pos, finishNode_Pos) => {
   for (let row = 0; row < NUMBER_ROW; row++) {
     const currentRow = [];
     for (let col = 0; col < NUMBER_COL; col++) {
-      currentRow.push(createNode(row,col,startNode_Pos, finishNode_Pos));
+      currentRow.push(createNode(row, col, startNode_Pos, finishNode_Pos));
     }
     matrix.push(currentRow);
   }
   return matrix;
 };
 
-
 const createNode = (row, col, startNode, finishNode) => {
-
   let start_x = startNode[0];
   let start_y = startNode[1];
   let finish_x = finishNode[0];
@@ -329,8 +374,8 @@ const createNode = (row, col, startNode, finishNode) => {
   return {
     col,
     row,
-    isStart: (row === start_x && col === start_y),
-    isFinish: (row === finish_x && col === finish_y),
+    isStart: row === start_x && col === start_y,
+    isFinish: row === finish_x && col === finish_y,
     distance: Infinity,
     isVisited: false,
     isWall: false,
@@ -338,45 +383,37 @@ const createNode = (row, col, startNode, finishNode) => {
   };
 };
 
-
-
 const MatrixDynamicNodes = (matrix, row, col, pos) => {
   console.log(`start node is currently at: row: ${row} col: ${col}`);
   let newMatrix = matrix.slice();
   const node = newMatrix[row][col];
   if (pos === "start") {
-      const newNode = {
-          ...node,
-          isStart: true
-      }
-      newMatrix[row][col] = newNode;
+    const newNode = {
+      ...node,
+      isStart: true,
+    };
+    newMatrix[row][col] = newNode;
   }
   if (pos === "finish") {
-      const newNode = {
-          ...node,
-          isFinish: true
-      }
-      newMatrix[row][col] = newNode;
+    const newNode = {
+      ...node,
+      isFinish: true,
+    };
+    newMatrix[row][col] = newNode;
   }
   return newMatrix;
-}
-
-
+};
 
 // Toggled and untoggle isWall to the current node
 const MatrixWithWallToggled = (matrix, row, col) => {
-
   let newMatrix = matrix.slice();
   const node = newMatrix[row][col];
-  
+
   const newNode = {
-      ...node,
-      isWall: !node.isWall
+    ...node,
+    isWall: !node.isWall,
   };
 
   newMatrix[row][col] = newNode;
   return newMatrix;
-}
-
-
-
+};
