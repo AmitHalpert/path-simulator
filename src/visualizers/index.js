f3cb29a26f2c11677adec0dd6e975f7933bfbfd3
amit <amit.ethan.halpert@gmail.com>
2022-08-07 14:40:16 +0300

    initial commit

--- src/visualizers/index.js
+++
@@ -1,7 +0,0 @@
-import { animatePath } from "./algorithms.visualizer";
-
-export { animatePath };
-
-export const setVisualizationState = (klass) => {
-  klass.setState({ isVisualizing: false  });
-}
